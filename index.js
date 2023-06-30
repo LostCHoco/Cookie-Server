6843113828a2a275df3fceda49107d03213bdf69
LostCHoco <chanho961125@gmail.com>
2023-06-30 16:33:15 +0900

    added password event
    방 비밀번호 체크 함수 추가

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ import { setID, updateUserlist } from "./roomRepository.js";
 const PORT = 3333;
 const io = new Server(PORT, {
   cors: {
-    origin: "https://cookiechat-4df6b.web.app",
+    origin: ["http://localhost:3000", "https://cookiechat-4df6b.web.app"],
   },
 });
 console.log(`server on => http://localhost:${PORT}`);
 const roomRepository = [];
 const roomID = [];
 const chatRepository = {};
-const users = [];
 
 function sendToAll(socket, key, data) {
   socket.broadcast.emit(key, data);
@@ -21,7 +20,7 @@ function sendToAll(socket, key, data) {
 //클라이언트에서 유저 접속 했을 때 이벤트 발생
 io.on("connection", (socket) => {
   // console.log(socket.id);
-  const ip = socket.handshake.address;
+  // const ip = socket.handshake.address;
   /*클라이언트에서 유저가 접속시
   서버에 저장되어있는 방들의 데이터 배열을 전송*/
   socket.on("req-initialize", () => {
@@ -36,7 +35,6 @@ io.on("connection", (socket) => {
     chatRepository[id].push(data);
     // 채팅메세지 송신
     sendToAll(socket, "chat-response", chatRepository);
-    console.log(chatRepository);
   });
   // 클라이언트에서 방 생성 요청
   socket.on("newRoom", (room) => {
@@ -53,31 +51,47 @@ io.on("connection", (socket) => {
   });
   //채팅방에 유저 입장
   socket.on("enter-room", (data) => {
-    console.log(1);
-
     const idx = roomRepository.findIndex((item) => item.id === data.id);
     // roomRepository[idx].userList = updateUserlist(
     //   roomRepository[idx].userList,
     //   data.user.uid
     // );
-    Object.defineProperty(roomRepository[idx].userList, data.user.uid, {
-      value: {
-        name: data.user.name,
-        photo: data.user.photo,
-      },
-      configurable: true,
-      enumerable: true,
-      writable: true,
+    const isNotOwner =
+      roomRepository[idx].userList[data.user.uid] === undefined;
+    if (isNotOwner) {
+      Object.defineProperty(roomRepository[idx].userList, data.user.uid, {
+        value: {
+          name: data.user.name,
+          photo: data.user.photo,
+          isLogin: false,
+        },
+        configurable: true,
+        enumerable: true,
+        writable: true,
+      });
+    }
+    if (roomRepository[idx].password === "") {
+      roomRepository[idx].userList[data.user.uid].isLogin = true;
+    }
+    const listArray = Object.values(roomRepository[idx].userList);
+    let loginCount = 0;
+    listArray.forEach((item) => {
+      if (item.isLogin) ++loginCount;
     });
-    const current = Object.keys(roomRepository[idx].userList).length;
-    roomRepository[idx].current = current;
+    roomRepository[idx].current = loginCount;
     sendToAll(socket, "update-room", roomRepository);
     // console.log("list", roomRepository[idx]);
     // console.log("info : ", userInfo);
   });
+  //방 비밀번호 입력 성공 이벤트
+  socket.on("pass-login", ([id, uid]) => {
+    const idx = roomRepository.findIndex((item) => item.id === id);
+    //data[0]:roomId, data[1]:UID
+    roomRepository[idx].userList[uid].isLogin = true;
+    sendToAll(socket, "update-room", roomRepository);
+  });
   //접속 해제 이벤트
-  socket.on("disconnect", (data) => {
+  socket.on("disconnect", () => {
     // console.log(socket.id, "disconnected");
-    users.splice(users.indexOf(socket.id), 1);
   });
 });
