eca3f2d70014e9b00187551c53f2a2fbf42eb416
LostCHoco <chanho961125@gmail.com>
2023-06-30 14:03:55 +0900

    fixed loading issue
    서버 무한로딩 문제 해결

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { setID, updateUserlist } from "./roomRepository.js";
 const PORT = 3333;
 const io = new Server(PORT, {
   cors: {
-    origin: ["http://localhost:3000", "https://cookiechat-4df6b.web.app"],
+    origin: "https://cookiechat-4df6b.web.app",
   },
 });
 console.log(`server on => http://localhost:${PORT}`);
@@ -29,11 +29,13 @@ function sendToAll(socket, key, data) {
 
 //클라이언트에서 유저 접속 했을 때 이벤트 발생
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  // console.log(socket.id);
   const ip = socket.handshake.address;
   /*클라이언트에서 유저가 접속시
   서버에 저장되어있는 방들의 데이터 배열을 전송*/
-  socket.emit("initialize", [roomRepository, chatRepository]);
+  socket.on("req-initialize", () => {
+    socket.emit("initialize", [roomRepository, chatRepository]);
+  });
   socket.emit("clientID", socket.id);
   // 채팅메세지 수신
   socket.on("chat-request", (msg) => {
@@ -60,7 +62,7 @@ io.on("connection", (socket) => {
   });
   //채팅방에 유저 입장
   socket.on("enter-room", (data) => {
-    console.log(data.user);
+    console.log(1);
 
     const idx = roomRepository.findIndex((item) => item.id === data.id);
     // roomRepository[idx].userList = updateUserlist(
@@ -79,12 +81,12 @@ io.on("connection", (socket) => {
     const current = Object.keys(roomRepository[idx].userList).length;
     roomRepository[idx].current = current;
     sendToAll(socket, "update-room", roomRepository);
-    console.log("list", roomRepository[idx]);
+    // console.log("list", roomRepository[idx]);
     // console.log("info : ", userInfo);
   });
   //접속 해제 이벤트
   socket.on("disconnect", (data) => {
-    console.log(socket.id, "disconnected");
+    // console.log(socket.id, "disconnected");
     users.splice(users.indexOf(socket.id), 1);
   });
 });
